f817bc3d030a834041889d6527b0eb4f093e8170
jpbandeiralima <jpbandeiralima@gmail.com>
2020-02-17 03:13:45 -0300

    Conclusão de execicio struct com function

--- activite-struct&&function/main.c
+++ activite-struct&&function/main.c
@@ -1,14 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const int QTD = 2;
+
 struct RegistrosCorrida {
     float frequencia;
     float temperatura;
     float elevacao;
-}registro[2];
+};
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+float calcularMedia(struct RegistrosCorrida *registroCorrida, int quantidadeParaCalculo){
+    int i; float soma = 0;
+    for(i = 0; i < quantidadeParaCalculo; i++){
+        soma = soma + registroCorrida[i].frequencia;
+    }
+    return soma / quantidadeParaCalculo;
+}
+
+float maiorFrequencia(struct RegistrosCorrida *registroCorrida, int quantidadeParaVetor){
+    int i; float maiorValor = 0;
+    for(i = 0; i < quantidadeParaVetor; i++){
+        if(registroCorrida[i].frequencia > maiorValor){
+            maiorValor = registroCorrida[i].frequencia;
+        }
+    }
+    return maiorValor;
+}
+
+void findByType(struct RegistrosCorrida *registroCorrida, int quantidadeParaVetor, int defineRetorno){
+    int i;
+    switch(defineRetorno){
+        case 1:
+            printf("Lista de FREQUENCIAS");
+                for(i = 0; i < quantidadeParaVetor; i++){
+                    printf("\nFrequencia: %f", registroCorrida[i].frequencia);
+        }break;
+        case 2:
+            printf("Lista de TEMPERATURAS");
+                for(i = 0; i < quantidadeParaVetor; i++){
+            printf("\Temperatura: %f", registroCorrida[i].temperatura);
+        }break;
+        case 3:
+            printf("Lista de ELECACOES");
+                for(i = 0; i < quantidadeParaVetor; i++){
+            printf("\nElevacao: %f", registroCorrida[i].elevacao);
+        }break;
+    }
+}
+
+int main(){
+
+    int i;
+    struct RegistrosCorrida *registroCorrida;
+    registroCorrida = malloc(QTD* sizeof(struct RegistrosCorrida));
+
+
+    for(i = 0; i < 2; i++){
+        printf("Digite a frequencia: ");
+        scanf("%f", &registroCorrida[i].frequencia);
+        printf("Digite a temperatura: ");
+        scanf("%f", &registroCorrida[i].temperatura);
+        printf("Digite a elevacao: ");
+        scanf("%f", &registroCorrida[i].elevacao);
+    }
+
+    findByType(registroCorrida, 2, 1);
+
+    printf("\nMedia: %f" , calcularMedia(registroCorrida, 2));
+    printf("\nMaior Frequencia: %f" , maiorFrequencia(registroCorrida, 2));
+
 }
--- activite3/main.c
+++ activite3/main.c
@@ -2,11 +2,21 @@
 #include <stdlib.h>
 
 void main(){
+    // Criada a variavel a e um ponteiro de b
     int a, *b;
     scanf("%i", &a);
+
+    //Apos essa linha o ponteiro b passara a apontar para a posicao de a.
     b = &a;
+
+    //Alterando o valor armazenado em a usando b para apontar para posicao e trocar o valor
     *b = 2*(*b);
+
+    //Apos alocar uma espaço de memoria para b, ele para de apontar para a posicao de a e passa a ter sua propria posicao
     b = malloc(sizeof(a));
+
     scanf("%i", b);
+
+    //No final do codigo, a deve ter seu valor inicial dobrado e b deve ficar com o valor digtado na linha 18
     printf("\n A: %i B: %i", a, *b);
 }
