446e81916c837ab18c2dc9ca4f4e489e405020d7
jpbandeiralima <jpbandeiralima@gmail.com>
2020-05-05 14:23:07 -0300

    recuperação de codigo perdido e reorganiza

---
+++ classes/04 - listas/lista-encadeada/simplesmente-encadeada/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "simples-encadeada.h"
+
+int main(int argc, int argv[]) {
+	TListaSimples* lista = criar(-1);
+
+	inserir(lista, 1);
+	inserir(lista, 2);
+	inserir(lista, 3);
+	inserir(lista, 4);
+
+	exibir(lista);
+
+	removerNaPosicao(lista, 3);
+	exibir(lista);
+
+	removerNaPosicao(lista, 1);
+	exibir(lista);
+
+	free(lista);
+
+	//printf("%i", tamanho(lista));
+}
